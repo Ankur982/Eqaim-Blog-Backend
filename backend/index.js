ab04f1b89ba98eb870481e904cd8d9b60abd186c
Ankur982 <singh999ankur@gmail.com>
2023-01-05 18:11:58 +0530

    adding Eqaim-Blog-backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const dbConnect = require("./dbConnect");
+const blogRoutes = require("./routes/blog")
+const cors = require("cors");
+
+
+dotenv.config();
+dbConnect();
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+
+app.use("/api", blogRoutes)
+
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server Started On Port 8080')
+});
---
+++ backend/routes/blog.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const multer = require("multer");
+const fs = require("fs")
+const Blog = require("../models/Blog");
+const { Server } = require("http");
+const express = require('express');
+
+
+router.post('/blog', async (req, res) => {
+    console.log(req.body)
+    try {
+        const newBlog = new Blog({
+            title: req.body.title,
+            summary: req.body.summary,
+            image: req.body.image
+        });
+        await newBlog.save();
+        res.status(200).send(newBlog);
+    } catch (err) {
+        res.status(500).json('Server Error....!');
+    }
+});
+
+
+
+router.get("/blog", async (req, res) => {
+    try {
+        const blogs = await Blog.find();
+        res.status(200).send(blogs);
+    } catch (e) {
+        res.status(404).send(e);
+    }
+});
+
+
+router.get("/blog/:id", async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        res.status(200).send(blog);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+
+
+
+module.exports = router;
